87cbef85fec0b32dc99152dda43c301bd92dbf06
Hudson Cooper <hncooper96@gmail.com>
2025-10-16 09:37:34 -0700

    tests

---
+++ sample_parser/tests/test_json_x_guidance.rs
@@ -0,0 +1,53 @@
+mod common_lark_utils;
+use common_lark_utils::lark_str_test;
+use serde_json::json;
+
+#[test]
+fn test_pattern_separators() {
+    let options = json!({
+        "item_separator": {"pattern": r"\s{0,2},\s{0,2}"},
+        "key_separator": {"pattern": r"\s{0,2}:\s{0,2}"},
+        "whitespace_flexible": false,
+    });
+    let lark = format!(
+        r#"
+        start: %json {{
+            "x-guidance": {options}
+        }}
+    "#
+    );
+    // Ok
+    lark_str_test(&lark, true, r#"{"a": 1, "b": 2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1,"b":2}"#, true);
+    lark_str_test(&lark, true, r#"{"a"  :  1 , "b":2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1 ,  "b":2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1,"b":  2}"#, true);
+    // Bad
+    lark_str_test(&lark, false, r#"{"a":1,   "b":2}"#, true);
+    lark_str_test(&lark, false, r#"{"a":1,"b":    2}"#, true);
+}
+
+#[test]
+fn test_literal_separators() {
+    let options = json!({
+        "item_separator": ", ",
+        "key_separator": ": ",
+        "whitespace_flexible": false,
+    });
+    let lark = format!(
+        r#"
+        start: %json {{
+            "x-guidance": {options}
+        }}
+    "#
+    );
+    // Ok
+    lark_str_test(&lark, true, r#"{"a": 1, "b": 2}"#, true);
+    // Bad
+    lark_str_test(&lark, false, r#"{"a":1,"b":2}"#, true);
+    lark_str_test(&lark, false, r#"{"a"  :  1 , "b":2}"#, true);
+    lark_str_test(&lark, false, r#"{"a":1 ,  "b":2}"#, true);
+    lark_str_test(&lark, false, r#"{"a":1,"b":  2}"#, true);
+    lark_str_test(&lark, false, r#"{"a":1,   "b":2}"#, true);
+    lark_str_test(&lark, false, r#"{"a":1,"b":    2}"#, true);
+}
