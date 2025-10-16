298badac58bb89325b2b1aee82ba6ddc4d97f887
Hudson Cooper <hncooper96@gmail.com>
2025-10-16 15:12:16 -0700

    tests

--- sample_parser/tests/test_json_x_guidance.rs
+++ sample_parser/tests/test_json_x_guidance.rs
@@ -3,32 +3,7 @@ use common_lark_utils::lark_str_test;
 use serde_json::json;
 
 #[test]
-fn test_pattern_separators() {
-    let options = json!({
-        "item_separator": {"pattern": r"\s{0,2},\s{0,2}"},
-        "key_separator": {"pattern": r"\s{0,2}:\s{0,2}"},
-        "whitespace_flexible": false,
-    });
-    let lark = format!(
-        r#"
-        start: %json {{
-            "x-guidance": {options}
-        }}
-    "#
-    );
-    // Ok
-    lark_str_test(&lark, true, r#"{"a": 1, "b": 2}"#, true);
-    lark_str_test(&lark, true, r#"{"a":1,"b":2}"#, true);
-    lark_str_test(&lark, true, r#"{"a"  :  1 , "b":2}"#, true);
-    lark_str_test(&lark, true, r#"{"a":1 ,  "b":2}"#, true);
-    lark_str_test(&lark, true, r#"{"a":1,"b":  2}"#, true);
-    // Bad
-    lark_str_test(&lark, false, r#"{"a":1,   "b":2}"#, true);
-    lark_str_test(&lark, false, r#"{"a":1,"b":    2}"#, true);
-}
-
-#[test]
-fn test_literal_separators() {
+fn test_simple_separators() {
     let options = json!({
         "item_separator": ", ",
         "key_separator": ": ",
@@ -50,4 +25,53 @@ fn test_literal_separators() {
     lark_str_test(&lark, false, r#"{"a":1,"b":  2}"#, true);
     lark_str_test(&lark, false, r#"{"a":1,   "b":2}"#, true);
     lark_str_test(&lark, false, r#"{"a":1,"b":    2}"#, true);
+}
+
+#[test]
+fn test_pattern_separators() {
+    let options = json!({
+        "item_separator": r"\s{0,2},\s{0,2}",
+        "key_separator": r"\s{0,2}:\s{0,2}",
+        "whitespace_flexible": false,
+    });
+    let lark = format!(
+        r#"
+        start: %json {{
+            "x-guidance": {options}
+        }}
+    "#
+    );
+    // Ok
+    lark_str_test(&lark, true, r#"{"a": 1, "b": 2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1,"b":2}"#, true);
+    lark_str_test(&lark, true, r#"{"a"  :  1 , "b":2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1 ,  "b":2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1,"b":  2}"#, true);
+    // Bad
+    lark_str_test(&lark, false, r#"{"a":1,   "b":2}"#, true);
+    lark_str_test(&lark, false, r#"{"a":1,"b":    2}"#, true);
+}
+
+#[test]
+fn test_flexible_separators() {
+    let options = json!({
+        "item_separator": r",",
+        "key_separator": r":",
+        "whitespace_flexible": true,
+    });
+    let lark = format!(
+        r#"
+        start: %json {{
+            "x-guidance": {options}
+        }}
+    "#
+    );
+    // Ok
+    lark_str_test(&lark, true, r#"{"a": 1, "b": 2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1,"b":2}"#, true);
+    lark_str_test(&lark, true, r#"{"a"  :  1 , "b":2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1 ,  "b":2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1,"b":  2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1,   "b":2}"#, true);
+    lark_str_test(&lark, true, r#"{"a":1,"b":    2}"#, true);
 }
